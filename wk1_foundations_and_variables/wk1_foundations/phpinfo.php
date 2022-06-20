2ac8c3978236555ccb11e5863a1c9f993c8f36fd
ben lamb <71160516+benlambm@users.noreply.github.com>
2022-06-20 14:28:12 -0400

    Add files via upload
    upload and package Wk1 code

---
+++ wk1_foundations_and_variables/wk1_foundations/phpinfo.php
@@ -0,0 +1,4 @@
+<?php include_once '../header.php';
+include_once 'menu.php';
+phpinfo();
+include_once '../footer.php';
---
+++ wk1_foundations_and_variables/wk1_variables/paula.php
@@ -0,0 +1,17 @@
+<?php include_once '../header.php';
+include_once './Employee.php';
+session_start();
+$pastorPaula = $_SESSION['Pt'];
+?>
+
+<div id="bio-page">
+    <h1>Park View Ministries</h1>
+    <img class="profile" src="<?php echo $pastorPaula->getPhotoUrl(); ?>" alt="employee profile">
+    <h2><?php echo $pastorPaula->getName().", ".$pastorPaula->getJobTitle();?></h2>
+    <blockquote><?php echo $pastorPaula->getBioInfo(); ?></blockquote>
+    <p>Paula has a <?php echo $pastorPaula->getDegree()."."; ?></p>
+    <p>Her passions include <?php echo $pastorPaula->getInterests()."."; ?></p>
+    <p>Her favorite book is <?php echo $pastorPaula->getFavBook()."."; ?></p>
+</div>
+
+<?php include_once '../footer.php'; ?>
