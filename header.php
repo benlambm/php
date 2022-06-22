96f5305f73475fc5faa272be4df5ffef79edf6e2
ben lamb <71160516+benlambm@users.noreply.github.com>
2022-06-22 16:40:35 -0400

    upload repeat inter-mod template files, stylesheet

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <title>CSIS410 Web Enterprise Technologies</title>
+        <meta name="description" content="Enterprise Web Development in PHP Code Homeworks">
+        <meta name="keywords" content="php web development enterprise technologies">
+        <meta charset="utf-8">
+
+        <link rel="stylesheet" type="text/css" href="http://php.ad4mtech.com/style.css" >
+
+    </head>
+
+    <body>
+        <main id="all-content">
+
