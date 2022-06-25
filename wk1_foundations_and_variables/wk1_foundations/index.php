e372a5d3145360a26d099d1364ad4db08f5d626d
ben lamb <71160516+benlambm@users.noreply.github.com>
2022-06-24 22:34:28 -0400

    delete old subdirectory

--- wk1_foundations_and_variables/wk1_foundations/index.php
+++
@@ -1,3 +0,0 @@
-<?php include_once '../header.php';
-include_once 'menu.php';
-include_once '../footer.php';
--- wk1_foundations_and_variables/wk1_foundations/phpinfo.php
+++
@@ -1,4 +0,0 @@
-<?php include_once '../header.php';
-include_once 'menu.php';
-phpinfo();
-include_once '../footer.php';
